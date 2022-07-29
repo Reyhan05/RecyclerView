30f9e9c44abed81eff36b3fce3c9a8bf33f2e239
reyhankelas7bputrabukyayah <reyhanmohamad0212@gmail.com>
2022-07-29 08:58:34 +0700

    Initial Commit

---
+++ app/src/main/java/com/reyhan/myapplication/firstrecyclerview/AnimalModel.kt
@@ -0,0 +1,3 @@
+package com.reyhan.myapplication.firstrecyclerview
+
+class AnimalModel
---
+++ app/src/main/java/com/reyhan/myapplication/firstrecyclerview/FirstActivity.kt
@@ -0,0 +1,12 @@
+package com.reyhan.myapplication.firstrecyclerview
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.reyhan.myapplication.R
+
+class FirstActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_first)
+    }
+}
